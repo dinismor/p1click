45434ab320fc1fcf3b5607bc1d3a919c86ac8165
Dinis Morais <dinismorais2000@gmail.com>
2021-07-21 22:24:51 +0100

    empty

--- src/view/auth-header.js
+++
@@ -1,8 +0,0 @@
-export default function authHeader() {
-  const login = JSON.parse(localStorage.getItem("user"));
-  if (user && user.token) {
-    return { Authorization: "Bearer " + user.token };
-  } else {
-    return {};
-  }
-}
